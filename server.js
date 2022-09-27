122f6b54651d7164dd164caff6066a41b14d30ee
NutProhmpiriya <105987695+NutProhmpiriya@users.noreply.github.com>
2022-09-28 00:46:39 +0700

    fix server 3

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-
 const middlewares = jsonServer.defaults()
+
 server.use(middlewares)
+server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
-    if (req.method === 'POST') {
-        req.body.createdAt = Date.now().toString()
+    const currentTime = new Date().toISOString()
 
+    if (req.method === "POST") {
+        req.body.createdAt = currentTime
+        req.body.modifiedAt = currentTime;
+    } else if (["PUT", "PATCH"].includes(req.method)) {
+        req.body.modifiedAt = currentTime;
     }
-    if (req.method === 'PUT' || 'PATCH') {
-        req.body.updatedAt = Date.now().toString()
-    }
-    next()
 
-})
-
+    next();
+});
 server.use(router)
 server.listen(3501, () => {
-    console.log(`JSON Server is running on port 3501`)
+    console.log('JSON Server is running 3501')
 })
